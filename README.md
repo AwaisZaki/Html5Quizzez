19b9585c3980858881d60a05a246d6359b01169b
Awais Zaki <Awais Zaki>
2016-06-01 01:41:59 +0500

    HTML5 Quizzez Part1

---
+++ README.md
@@ -0,0 +1,26 @@
+# **HTML5 Quizzez**
+
+### This quiz test will improve your knowledge about html5, Click the 'Submit Test' button given in the bottom of this page to Submit your answers _[HTML5 Quizzez](https://html5quizzez.firebaseapp.com/)_ .
+
+#### MORE QUIZZEZ COMMING SOON
+
+### Click the link to view the page
+
+[HTML5 Quizzez](https://html5quizzez.firebaseapp.com/)
+
+I used HTML, CSS and Basic scripting language(Javascript) in this page.
+
+
+
+
+* [Github](https://github.com/AwaisZaki)
+
+* [Linkedin](https://www.linkedin.com/in/awais-zaki-5104b755?trk=hp-identity-name)
+
+* [Facebook](https://www.facebook.com/owais.zaki)
+
+* [Google+](https://plus.google.com/u/0/100388485894883304247)
+
+* [Branded.me](https://branded.me/home)
+
+* [Opportunity](http://www.myopportunity.com/leads/662879)
---
+++ quiz-GetFormResult.js
@@ -0,0 +1,49 @@
+function quizresult(form){
+    
+    
+    
+    
+    alert("Your Quiz has been Submitted Successfuly!")
+    
+   document.write("<body style='background-image: url(web.jpg);'></body>")
+    document.write("<h1 style='text-align:center; color:#FF00FF'>Submitted Successfuly </h1>"+ "<h3 style='text-align:center; color: #FFA500'>Click on ANSWER to see answer</h3>" + "<a target='_blank' href='answer.html' style='text-decoration:none; color: Red;'><h1 style='text-align:center' >ANSWER </h1></a><h1 style='text-align:center; color:#7CFC00'>More Quizzes Comming Soon</h1>");
+    
+    
+    
+    
+    
+    document.write("<center><span ><a href='https://www.facebook.com/owais.zaki'><img src='facebook_circle.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 0px;'/></a></span> <span ><a href='https://twitter.com/AwaisZaki9'><img src='twitter_circle.png' style='background-repeat: no-repeat; background-size:30px; height: 50px;  width: 50px; background-position: center; margin-left: 30px;'/></a></span> <span ><a href='https://www.linkedin.com/in/awais-zaki-5104b755?trk=hp-identity-name'><img src='linkdin.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 30px;'/></a></span> <span ><a href='https://www.facebook.com/owais.zaki'><img src='gmail1.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 30px;'/></a></span> <span ><a href='https://edit.yahoo.com/config/eval_profile'><img src='yahoo.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 30px;'/></a></span> <span ><a href='https://plus.google.com/u/0/100388485894883304247'><img src='google_circle.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 30px;'/></a></span> <span ><a href='https://github.com/AwaisZaki'><img src='gh.png' style='background-repeat: no-repeat; background-size:30px; height: 50px; width: 50px; background-position: center; margin-left: 30px;'/></a></span></center>'");
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function refresh(){
+    
+    location.reload();
+    
+    
+    
+    
+}
